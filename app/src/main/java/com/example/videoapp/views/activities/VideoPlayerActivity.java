1a2fa04fc5fde41d789f985a2a52dbc6fab048be
Jonathan Havstad <jjhavstad@gmail.com>
2017-04-14 07:52:47 -0700

    Adding support for JWPlayer

--- app/src/main/java/com/example/videoapp/views/activities/VideoPlayerActivity.java
+++ app/src/main/java/com/example/videoapp/views/activities/VideoPlayerActivity.java
@@ -3,12 +3,18 @@ package com.example.videoapp.views.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.example.videoapp.R;
 import com.example.videoapp.models.data.VideoData;
+import com.example.videoapp.models.data.VideoSource;
 import com.example.videoapp.presenters.AppPresenter;
 import com.example.videoapp.views.fragments.ExoPlayerFragment;
+import com.longtailvideo.jwplayer.JWPlayerFragment;
+import com.longtailvideo.jwplayer.JWPlayerSupportFragment;
+import com.longtailvideo.jwplayer.configuration.PlayerConfig;
 
 /**
  * Created by jonathanhavstad on 4/13/17.
@@ -24,17 +30,32 @@ public class VideoPlayerActivity extends FragmentActivity {
         VideoData videoData = intent.getParcelableExtra(getString(R.string.video_data_key));
         AppPresenter.PLAYER_TYPE playerType =
                 AppPresenter.PLAYER_TYPE.valueOf(intent.getStringExtra(getString(R.string.player_type_key)));
+        Fragment playerFrament = null;
         switch (playerType) {
             case EXO_PLAYER:
-                ExoPlayerFragment exoPlayerFragment = ExoPlayerFragment.newInstance(this, dashUrl, videoData);
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .add(R.id.video_player_view, exoPlayerFragment)
-                        .commit();
+                playerFrament = ExoPlayerFragment.newInstance(this, dashUrl, videoData);
+                Toast.makeText(this, R.string.exo_player_toast_message, Toast.LENGTH_SHORT).show();
                 break;
             case JW_PLAYER:
+                StringBuffer videoUri = new StringBuffer();
+                videoUri.append(dashUrl);
+                for (VideoSource videoSource : videoData.getSources()) {
+                    if (videoSource.getType().equals(getString(R.string.dash_video_source_key))) {
+                        videoUri.append(videoSource.getUrl());
+                    }
+                }
+                PlayerConfig playerConfig =
+                        new PlayerConfig.Builder().file(videoUri.toString()).build();
+                playerFrament = JWPlayerSupportFragment.newInstance(playerConfig);
+                Toast.makeText(this, R.string.jw_player_toast_message, Toast.LENGTH_SHORT).show();
                 break;
             default:
+        }
+        if (playerFrament != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.video_player_view, playerFrament)
+                    .commit();
         }
     }
 }
