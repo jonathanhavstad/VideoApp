8b5ef1026072642128c5224cf2bd2938ec3e00f5
Jonathan Havstad <jjhavstad@gmail.com>
2017-04-16 22:45:12 -0400

    Adding support to resume playback after an orientation change and to resume from same position

--- app/src/main/java/com/example/videoapp/views/activities/VideoPlayerActivity.java
+++ app/src/main/java/com/example/videoapp/views/activities/VideoPlayerActivity.java
@@ -1,11 +1,12 @@
 package com.example.videoapp.views.activities;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.videoapp.R;
@@ -21,46 +22,105 @@ import com.longtailvideo.jwplayer.configuration.PlayerConfig;
  */
 
 public class VideoPlayerActivity extends FragmentActivity {
+    private static final String TAG = "VideoPlayerActivity";
+    private static final String EXO_PLAYER_FRAGMENT_TAG = "ExoPlayerFragment";
+    private static final String JW_PLAYER_FRAGMENT_TAG = "JwPlayerFragment";
+
+    private String fragmentTag;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_video_player);
-        Intent intent = getIntent();
-        String dashUrl = intent.getStringExtra(getString(R.string.dash_url_key));
-        VideoData videoData = intent.getParcelableExtra(getString(R.string.video_data_key));
-        AppPresenter.PLAYER_TYPE playerType =
-                AppPresenter.PLAYER_TYPE.valueOf(intent.getStringExtra(getString(R.string.player_type_key)));
-        Fragment playerFrament = null;
-        switch (playerType) {
-            case EXO_PLAYER:
-                playerFrament = ExoPlayerFragment.newInstance(this, dashUrl, videoData);
-                Toast.makeText(this, R.string.exo_player_toast_message, Toast.LENGTH_SHORT).show();
-                break;
-            case JW_PLAYER:
-                StringBuffer videoUri = new StringBuffer();
-                videoUri.append(dashUrl);
-                for (VideoSource videoSource : videoData.getSources()) {
-                    if (videoSource.getType().equals(getString(R.string.dash_video_source_key))) {
-                        videoUri.append(videoSource.getUrl());
+        if (savedInstanceState == null) {
+            Intent intent = getIntent();
+            String dashUrl = intent.getStringExtra(getString(R.string.dash_url_key));
+            VideoData videoData = intent.getParcelableExtra(getString(R.string.video_data_key));
+            AppPresenter.PLAYER_TYPE playerType =
+                    AppPresenter.PLAYER_TYPE.valueOf(intent.getStringExtra(getString(R.string.player_type_key)));
+            Fragment playerFragment = null;
+            boolean createFragment = false;
+            fragmentTag = null;
+            switch (playerType) {
+                case EXO_PLAYER:
+                    fragmentTag = EXO_PLAYER_FRAGMENT_TAG;
+                    if (getSupportFragmentManager().findFragmentByTag(fragmentTag) == null) {
+                        playerFragment = ExoPlayerFragment.newInstance(this, dashUrl, videoData);
+                        createFragment = true;
                     }
-                }
-                PlayerConfig playerConfig =
-                        new PlayerConfig.Builder().file(videoUri.toString()).build();
-                playerFrament = JWPlayerSupportFragment.newInstance(playerConfig);
-                Toast.makeText(this, R.string.jw_player_toast_message, Toast.LENGTH_SHORT).show();
-                break;
-            default:
-        }
-        if (playerFrament != null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.video_player_view, playerFrament)
-                    .commit();
+                    Toast.makeText(this, R.string.exo_player_toast_message, Toast.LENGTH_SHORT).show();
+                    break;
+                case JW_PLAYER:
+                    fragmentTag = JW_PLAYER_FRAGMENT_TAG;
+                    if (getSupportFragmentManager().findFragmentByTag(fragmentTag) == null) {
+                        StringBuffer videoUri = new StringBuffer();
+                        videoUri.append(dashUrl);
+                        for (VideoSource videoSource : videoData.getSources()) {
+                            if (videoSource.getType().equals(getString(R.string.dash_video_source_key))) {
+                                videoUri.append(videoSource.getUrl());
+                            }
+                        }
+                        PlayerConfig playerConfig =
+                                new PlayerConfig.Builder().file(videoUri.toString()).build();
+                        playerFragment = JWPlayerSupportFragment.newInstance(playerConfig);
+                        createFragment = true;
+                    }
+                    Toast.makeText(this, R.string.jw_player_toast_message, Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+            }
+            if (createFragment) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.video_player_view, playerFragment, fragmentTag)
+                        .commit();
+            }
+        } else {
+
         }
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        if (isFinishing()) {
+            FragmentManager fm = getSupportFragmentManager();
+            Fragment currentFragment = fm.findFragmentByTag(fragmentTag);
+            if (currentFragment != null) {
+                fm.beginTransaction().remove(currentFragment).commit();
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
--- app/src/main/java/com/example/videoapp/views/fragments/ExoPlayerFragment.java
+++ app/src/main/java/com/example/videoapp/views/fragments/ExoPlayerFragment.java
@@ -3,8 +3,11 @@ package com.example.videoapp.views.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +15,7 @@ import android.view.ViewGroup;
 import com.example.videoapp.R;
 import com.example.videoapp.models.data.VideoData;
 import com.example.videoapp.models.data.VideoSource;
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
@@ -29,6 +33,7 @@ import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
@@ -39,9 +44,48 @@ import butterknife.ButterKnife;
  */
 
 public class ExoPlayerFragment extends Fragment {
+    private static final String TAG = "ExoPlayerFragment";
+
     private SimpleExoPlayer simpleExoPlayer;
     private String dashUrl;
     private VideoData videoData;
+    private SavedState savedState;
+
+    public static class SavedState implements Parcelable {
+        private boolean isPlaying;
+        private int resumeWindow;
+        private long resumePosition;
+
+        protected SavedState(Parcel in) {
+            isPlaying = in.readByte() != 0;
+            resumeWindow = in.readInt();
+            resumePosition = in.readLong();
+        }
+
+        public static final Creator<SavedState> CREATOR = new Creator<SavedState>() {
+            @Override
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            @Override
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeByte((byte) (isPlaying ? 1 : 0));
+            dest.writeInt(resumeWindow);
+            dest.writeLong(resumePosition);
+        }
+    }
 
     @BindView(R.id.exoplayer_view)
     SimpleExoPlayerView simpleExoPlayerView;
@@ -56,12 +100,34 @@ public class ExoPlayerFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach");
+        Parcel parcel = Parcel.obtain();
+        parcel.writeByte((byte) 0);
+        parcel.writeInt(C.INDEX_UNSET);
+        parcel.writeLong(C.TIME_UNSET);
+        savedState = SavedState.CREATOR.createFromParcel(parcel);
+    }
+
+    @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
 
         Bundle args = getArguments();
         dashUrl = args.getString(getString(R.string.dash_url_key));
         videoData = args.getParcelable(getString(R.string.video_data_key));
+
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
+        View view = inflater.inflate(R.layout.fragment_exoplayer, container, false);
+        ButterKnife.bind(this, view);
 
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
         TrackSelection.Factory videoTrackSelectionFactory =
@@ -71,9 +137,16 @@ public class ExoPlayerFragment extends Fragment {
         LoadControl loadControl = new DefaultLoadControl();
         simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
 
+        simpleExoPlayerView.setPlayer(simpleExoPlayer);
+
+        DataSource.Factory mediaDataSourceFactor = new DefaultDataSourceFactory(getContext(),
+                (DefaultBandwidthMeter) bandwidthMeter,
+                new DefaultHttpDataSourceFactory(
+                        Util.getUserAgent(getContext(), getString(R.string.video_app_name)),
+                        (DefaultBandwidthMeter) bandwidthMeter));
         DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getContext(),
-                Util.getUserAgent(getContext(), getString(R.string.video_app_name)),
-                (DefaultBandwidthMeter) bandwidthMeter);
+                null,
+                new DefaultHttpDataSourceFactory(Util.getUserAgent(getContext(), getString(R.string.video_app_name)), null));
         StringBuffer videoUri = new StringBuffer();
         videoUri.append(dashUrl);
         for (VideoSource videoSource : videoData.getSources()) {
@@ -82,27 +155,83 @@ public class ExoPlayerFragment extends Fragment {
             }
         }
         DashChunkSource.Factory dashChunkSourceFactory =
-                new DefaultDashChunkSource.Factory(dataSourceFactory);
+                new DefaultDashChunkSource.Factory(mediaDataSourceFactor);
         MediaSource videoSource = new DashMediaSource(Uri.parse(videoUri.toString()),
                 dataSourceFactory,
                 dashChunkSourceFactory,
                 null,
                 null);
         simpleExoPlayer.prepare(videoSource);
+
+        if (savedInstanceState != null) {
+            savedState = savedInstanceState.getParcelable(getString(R.string.player_state_key));
+            if (savedState != null) {
+                simpleExoPlayer.setPlayWhenReady(savedState.isPlaying);
+                if (savedState.resumeWindow != C.INDEX_UNSET) {
+                    simpleExoPlayer.seekTo(savedState.resumeWindow, savedState.resumePosition);
+                }
+            }
+        }
+
+        return view;
     }
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_exoplayer, container, false);
-        ButterKnife.bind(this, view);
-        simpleExoPlayerView.setPlayer(simpleExoPlayer);
-        return view;
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        savedState.isPlaying = simpleExoPlayer.getPlayWhenReady();
+        savedState.resumeWindow = simpleExoPlayer.getCurrentWindowIndex();
+        savedState.resumePosition = simpleExoPlayer.isCurrentWindowSeekable() ?
+                Math.max(0, simpleExoPlayer.getCurrentPosition()) : C.TIME_UNSET;
+        outState.putParcelable(getString(R.string.player_state_key), savedState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView");
+        simpleExoPlayer.release();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        simpleExoPlayer.release();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach");
     }
 }
